c82527532b78cffd733d357bbfc2594785167805
ReihanMutaqin <reyhanmutakin1@gmail.com>
2026-07-13 19:06:38 +0700

    Use modern Vercel Web Standard Handlers instead of legacy hono/vercel adapter

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
-import { handle } from "hono/vercel";
 import app from "../dist/boot.js";
 
-export default handle(app);
+export const GET = app.fetch;
+export const POST = app.fetch;
+export const PUT = app.fetch;
+export const PATCH = app.fetch;
+export const DELETE = app.fetch;
+export const OPTIONS = app.fetch;
