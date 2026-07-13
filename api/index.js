7862055d5d56abb9e84c8f6f5cce73cb04c105d7
ReihanMutaqin <reyhanmutakin1@gmail.com>
2026-07-13 18:21:21 +0700

    Fix Vercel TS build errors by using bundled boot.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import { handle } from "hono/vercel";
-import app from "../server/boot";
+import app from "../dist/boot.js";
 
 // Export the Hono app as a Vercel Serverless Function
 export default handle(app);
